babae3c9204a6a1591efe49a639b551a7eeb7fbd
Fodrick Nyirongo <nyirongofodrick@gmail.com>
2024-10-23 18:26:41 +0200

    added user messages on console

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from FlightRoutes import FlightRoutes
 
 
 class Main:
-    
-    # dummy runs
+    # Data scrapped from provided graph
     airport_list = ['JFK', 'HND', 'ICN', 'EWR', 'LGA', 'BGI', 'ORD', 'DSM', 'SFO', 'SAN', 'EYW', 'LHR', 'TLV', 'DEL',
-                    'DOH',
-                    'CDG', 'SIN', 'BUD']  # Airport list scrapped from the graph given
+                    'DOH', 'CDG', 'SIN', 'BUD']  # Airport list scrapped from the graph given
 
     flight_routes = [('EWR', 'HND'), ('HND', 'ICN'), ('ICN', 'JFK'), ('HND', 'JFK'), ('JFK', 'LGA'), ('BGI', 'LGA'),
                      ('ORD', 'BGI'), ('DSM', 'ORD'),
@@ -14,10 +12,20 @@ class Main:
                      ('TLV', 'DEL'), ('DEL', 'DOH'), ('DEL', 'CDG'), ('CDG', 'BUD'), ('CDG', 'SIN'), ('SIN', 'CDG')
                      ]  # Routes taken from the graph given
 
-    start_airport = 'ICN'
+    loop_control = 0
 
-    flight_planner = FlightRoutes(airport_list, flight_routes, start_airport)
+    while loop_control == 0:
+        # To ensure the code a user enter is converted to capital and reduce errors
+        start_airport = input("\nEnter Starting Airport id using the airport code for me to analyse for you: ").upper()
 
-    result = flight_planner.find_min_routes_to_reach_all()
+        if start_airport not in airport_list:
+            print(f'Oops!!! {start_airport} airport is not in list. Try another airport code \n')
+        else:
+            flight_planner = FlightRoutes(airport_list, flight_routes, start_airport)
+            result = flight_planner.find_min_routes_to_reach_all()
+            # Display result to the user with friendly message
+            print(f'Starting from {start_airport}, minimum additional one-way routes needed are: {result} \n')
+            user_choice = input('type Q to exit. Or type any letter to  Try again ').upper()
 
-    print(result)
+            if user_choice == 'Q':
+                loop_control = 1
