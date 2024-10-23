b3ce198bddb26b6608feaf61c0b9d639bbfcddfa
Fodrick Nyirongo <nyirongofodrick@gmail.com>
2024-10-23 15:19:43 +0200

    added reverse depth first method

--- FlightRoutes.py
+++ FlightRoutes.py
@@ -12,7 +12,19 @@ class FlightRoutes:
         visited.add(node)
         for nodeItem in graph[node]:
             if nodeItem not in visited:
+                # This is sort of a recursive call
                 self.do_depth_first(graph, nodeItem, visited, stack)
         stack.append(node)
 
     # Perform the Depth First transversing in the reversed graph
+    def do_reverse_depth_first(self, graph, node, visited, component):
+        # each visited note is added to the visited list to keep track
+        visited.add(node)
+        # Mark the strongly connected component
+        component.append(node)
+        for graphNodeItem in graph[node]:
+            if graphNodeItem not in visited:
+                # recursive call of the do_reverse_depth_first
+                self.do_reverse_depth_first(graph, graphNodeItem, visited, component)
+
+    
